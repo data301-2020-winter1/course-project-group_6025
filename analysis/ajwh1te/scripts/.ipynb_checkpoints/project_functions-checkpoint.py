6b1b4d2a47922e7cfb85025494b2c7e09062b5b5
Austin White <austinjwhite123@gmail.com>
2020-11-11 12:13:27 -0800

    Updated .py file as the proper format

---
+++ analysis/ajwh1te/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+def load_and_process(path):
+    df1 = (
+        pd.read_csv("../../../data/raw/DataSet_ajwh1te.csv")
+        .dropna(how='any')
+        .rename(columns={"created_at":"time_start", "last_move_at":"time_end"})
+        .sort_values("turns", ascending=True)
+        )
+    df2 = (df1
+           .assign(date_start=(pd.to_datetime(df1["time_start"])))
+           .assign(date_end=(pd.to_datetime(df1["time_end"])))
+          )
+    df3 = (
+        df2
+        .assign(length=(df2["date_end"] - df2["date_start"])*10000000)
+        .drop(columns=["white_id",
+                          "id",
+                          "black_id",
+                          "increment_code",
+                          "time_start",
+                          "time_end",
+                          "rated"])
+          )
+    return df3
