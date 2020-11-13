bc3208c819825cfc26d3d078a48b614669d6553a
akshatsingal00@gmail.com <akshatsingal00@gmail.com>
2020-11-12 23:42:26 -0800

    finished individual analysis

--- analysis/akshat/scripts/rating.py
+++ analysis/akshat/scripts/rating.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def modify(df):
-    abs_rating = pd.series([])
+    abs_rating = pd.Series([])
     
     for i in range(len(df)):
         abs_rating[i] = abs(df["white_rating"][i] - df["black_rating"][i])
    
-    df1 = (df.insert(4, "absolute_rating", abs_rating))
+    (df.insert(4, "absolute_rating", abs_rating))
 
-    return df1 
+    return df
