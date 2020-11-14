1c791a1f9e0ee0cebbab5ce96458704bc31ddd8c
Ian Robinson <robinson.iandavid@gmail.com>
2020-11-13 17:39:24 -0800

    updated .py file

--- analysis/Robiian1989/scripts/.ipynb_checkpoints/dataproject_functions1-checkpoint.py
+++ analysis/Robiian1989/scripts/.ipynb_checkpoints/dataproject_functions1-checkpoint.py
@@ -13,7 +13,7 @@ def load_and_process(path):
     df2= (df1.drop(['created_at','last_move_at','rated','white_id','black_id'],axis=1)
         .dropna()
         .sort_values(by=['turns'],ascending=True)
-        .assign(move_name_and_eco= df['opening_name']+ '('+ df['opening_eco'] +')')
+        .assign(move_name_and_eco= df1['opening_name']+ '('+ df1['opening_eco'] +')')
         .drop(['opening_eco','opening_name'],axis=1)
         .rename(columns={'increment_code':'time'})
         .reset_index(drop=True)
